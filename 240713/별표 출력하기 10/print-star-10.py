503eb3c64ae67038258c6aef95cc1267041e4213
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-13 23:57:15 +0900

    Title: 별표 출력하기 10, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240713/별표 출력하기 10/print-star-10.py
+++ 240713/별표 출력하기 10/print-star-10.py
@@ -2,27 +2,27 @@ n = int(input())
 
 cnt = 1
 cnt2 = n
+
+li = []
+
 for i in range(0,n):
     if i % 2 == 0 :
+        li_ = ""
         for j in range(0,cnt):
             print('*',end=' ')
+            li_ = li_ + '* '
+        li.append(li_)
         cnt = cnt + 1
     else:
+        li_ = ""
         for j in range(cnt2,0,-1):
             print('*',end=' ')
+            li_ = li_ + '* '
+        li.append(li_)
         cnt2 = cnt2 -1 
     print()
 
-cnt = cnt - 1
-cnt2 = cnt2 + 1
+li2 = li.reverse()
 
-for i in range(0,n):
-    if i % 2 == 0 :
-        for j in range(cnt,0,-1):
-            print('*',end=' ')
-        cnt = cnt - 1
-    else:
-        for j in range(0,cnt2):
-            print('*',end=' ')
-        cnt2 = cnt2 + 1 
-    print()
+for a in li:
+    print(a)
