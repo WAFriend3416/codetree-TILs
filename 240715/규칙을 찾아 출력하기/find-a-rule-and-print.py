f23074d7bf4ed001c446e28a307d8d8224a6e433
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-15 16:30:28 +0900

    Title: 규칙을 찾아 출력하기, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+cnt = n-2
+cnt2 = 0
+
+print(cnt)
+if n == 1:
+    print('*')
+else:
+    
+    for j in range(0,n):
+        print('*',end =' ')
+    print()
+    for k in range(0,n-2):
+        print('*',end =' ')
+        for b in range(0,cnt2):
+            print('*',end =' ')
+        for m in range(0,cnt):
+            print(' ',end =' ')
+        cnt = cnt - 1
+        cnt2 = cnt2 + 1
+        print('*',end =' ')
+        print()
+    for z in range(0,n):
+        print('*',end =' ')
