ebacecab10dfd380ecfb78e5638aa683c44f3f33
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-16 11:52:12 +0900

    Title: 두 숫자의 차의 최솟값, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 240716/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -5,7 +5,11 @@ li_result = []
 for i in range(1,n-1):
     curV1 = abs(li[i-1] - li[i])
     curV2 = abs(li[i] - li[i+1])
-    li_result.append(curV1)
-    li_result.append(curV2)
+    if curV1 in li_result or curV2 in li_result:
+        continue
+    
+    else:
+        li_result.append(curV1)
+        li_result.append(curV2)
 
 print(min(li_result))
