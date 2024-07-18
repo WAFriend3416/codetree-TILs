4838a635f22b76edd8081e58450b2fe33b2dfcf8
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-18 11:57:29 +0900

    Title: 미는 횟수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240718/미는 횟수/number-of-pushes.py
@@ -0,0 +1,20 @@
+n1 = list(input())
+n2 = list(input())
+
+cnt = 0
+for k  in range(len(n1)):
+    result = []
+    for M in n1:
+        result.append(M)
+
+    cnt = cnt + 1
+    for i in range(1,len(n1)):
+        n1[i] = result[i-1]
+    n1[0] = result[-1]
+
+    if n1 == n2:
+        print(cnt)
+        break
+
+if k == len(n1):
+    print(-1)
