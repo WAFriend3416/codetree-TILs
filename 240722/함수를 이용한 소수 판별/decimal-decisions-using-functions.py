d28da6d6f7a9760d9761a64d658f1cf66422978a
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-22 17:20:48 +0900

    Title: 함수를 이용한 소수 판별, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240722/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,16 @@
+def check_dec(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
+
+
+n,m = map(int,input().split())
+result = 0
+for i in range(n,m+1):
+    if check_dec(i):
+        result = result + i
+    else:
+        continue
+
+print(result)
