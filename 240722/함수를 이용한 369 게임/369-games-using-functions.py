06c01dced4743cd2d0b8b569d805cf6ba81ccee3
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-22 17:16:56 +0900

    Title: 함수를 이용한 369 게임, Time: 325ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240722/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,17 +1,22 @@
-def check_num_in3(n):
-    n = list(map(int,str(num)))
-    li = [3,6,9]
-    return n in li
+def check_in369(n):
+    li = list(map(int,str(n)))
+    if 3 in li or 6 in li or 9 in li:
+        return True
+    else:
+        return False
 
-def check_num_mul3(n):
+def check_mul3(n):
     return n%3 == 0
 
-cnt = 0 
+
+cnt = 0
 
 n,m = map(int,input().split())
 
 for i in range(n,m+1):
-    if check_num_in3(i) or check_num_mul3(i):
+    if check_in369(i) or check_mul3(i):
         cnt = cnt + 1
+    else:
+        continue
 
 print(cnt)
