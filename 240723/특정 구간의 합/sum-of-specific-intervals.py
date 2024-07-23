f1b20800a708cf9be336bd19db18b0e3ebf0c81e
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-23 10:40:09 +0900

    Title: 특정 구간의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240723/특정 구간의 합/sum-of-specific-intervals.py
+++ 240723/특정 구간의 합/sum-of-specific-intervals.py
@@ -5,6 +5,6 @@ li = list(map(int,input().split()))
 for _ in range(4):
     a,b = map(int,input().split())
     total = 0
-    for i in range(a-1,b):
+    for i in range(a,b+1):
         total += li[i]
     print(total)
