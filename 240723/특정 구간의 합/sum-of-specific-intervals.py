764116e617e29c659a22adffea2c433469f6443c
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-23 10:41:28 +0900

    Title: 특정 구간의 합, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240723/특정 구간의 합/sum-of-specific-intervals.py
+++ 240723/특정 구간의 합/sum-of-specific-intervals.py
@@ -5,6 +5,8 @@ li = list(map(int,input().split()))
 for _ in range(4):
     a,b = map(int,input().split())
     total = 0
-    for i in li[a-1:b]:
+    a = a-1
+    b = b-1
+    for i in li[a:b]:
         total += i
     print(total)
