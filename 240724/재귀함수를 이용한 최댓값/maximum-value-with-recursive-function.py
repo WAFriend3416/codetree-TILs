862525cf272b4a51355eb6241b396affa532438d
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-24 15:12:19 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,15 @@
+def search_max(N,n,k):
+    if N == n:
+        return k
+
+    if li[N] >= k:
+        k = li[N]
+
+    return search_max(N+1,n,k) 
+
+
+n = int(input())
+
+li = list(map(int,input().split()))
+
+print(search_max(0,6,0))
