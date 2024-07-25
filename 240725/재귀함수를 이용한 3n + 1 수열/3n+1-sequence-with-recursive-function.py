e12a8c5880dfb8a797fcfa275b0460f2e515be58
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-07-25 18:36:08 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
+++ 240725/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
@@ -1,13 +1,15 @@
-global cnt = 0
+cnt = 0
 
 def _3n_1(N):
-    cnt = cnt + 1
+    global cnt
     if N == 1:
         return
+    cnt = cnt + 1    
     if N % 2 == 0:
         return _3n_1(N//2)
     else:
         return _3n_1((N*3) + 1)
+    
 
 _3n_1(int(input()))
 print(cnt)
