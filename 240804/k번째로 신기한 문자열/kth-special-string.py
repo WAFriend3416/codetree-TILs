7cac8bb6e8244dcb761a72bb6a120d06455f4821
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-04 11:51:20 +0900

    Title: k번째로 신기한 문자열, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/k번째로 신기한 문자열/kth-special-string.py
+++ 240804/k번째로 신기한 문자열/kth-special-string.py
@@ -4,9 +4,9 @@ li_words = []
 
 for _ in range(int(li_item[0])):
     word = input()
-    if li_item[2] in word:
+    if word.startswith(li_item[2]):
         li_words.append(word)
 
-li_words
+li_words = sorted(li_words)
 
-print(sorted(li_words)[int(li_item[1])-1])
+print(li_words[int(li_item[1])-1])
