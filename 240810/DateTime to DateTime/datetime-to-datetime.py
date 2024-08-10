2624d480ce23d590119f3a32f731f9c21ff677b0
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-10 21:13:06 +0900

    Title: DateTime to DateTime, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/DateTime to DateTime/datetime-to-datetime.py
+++ 240810/DateTime to DateTime/datetime-to-datetime.py
@@ -2,9 +2,9 @@ day,hour,mint = map(int,input().split())
 
 if day < 11:
     print(-1)
-elif hour < 11:
+elif day < 11 and hour < 11:
     print(-1)
-elif hour < 11 and mint < 11:
+elif day < 11 and hour < 11 and mint < 11:
     print(-1)
 else:
     DAY = (day - 11) * 24 * 60
