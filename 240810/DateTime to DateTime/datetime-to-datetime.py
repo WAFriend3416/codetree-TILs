45cf9bf7beb38b23109f9a7d127e676a19f5a4e4
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-10 21:16:32 +0900

    Title: DateTime to DateTime, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/DateTime to DateTime/datetime-to-datetime.py
+++ 240810/DateTime to DateTime/datetime-to-datetime.py
@@ -1,14 +1,20 @@
-day,hour,mint = map(int,input().split())
-
-if day < 11:
-    print(-1)
-elif day < 11 and hour < 11:
-    print(-1)
-elif day < 11 and hour < 11 and mint < 11:
-    print(-1)
-else:
+def cal_min(DAY,HOUR,MIN):
     DAY = (day - 11) * 24 * 60
     HOUR = (hour - 11) * 60
     MIN = mint - 11
 
     print(DAY + HOUR + MIN)
+
+day,hour,mint = map(int,input().split())
+
+if day < 11:
+    print(-1)
+else:
+    if hour < 11:
+        print(-1)
+    else:
+        if hour >= 11 and mint < 11:
+            print(-1)
+
+        else:
+            cal_min(day,hour,mint)
