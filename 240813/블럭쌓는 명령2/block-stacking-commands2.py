ba8aaf3d09f2229354321bd883d1802fc53b32b6
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-13 18:37:52 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240813/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,16 @@
+n,m = map(int,input().split())
+
+li = [
+    0 for _ in range(n)
+]
+
+for i in range(m):
+    n,m = map(int,input().split())
+    if n == m:
+        li[n] += 1
+    else:
+        for i in range(n,m+1):
+            li[i] += 1
+    #print(li)
+
+print(max(li))
