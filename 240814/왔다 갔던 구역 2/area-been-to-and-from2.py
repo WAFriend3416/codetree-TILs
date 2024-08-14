c9d27717eb9d364797bd127702f3776307812312
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-14 17:31:19 +0900

    Title: 왔다 갔던 구역 2, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240814/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240814/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,24 +1,49 @@
-SIZE = 2000
+OFFSET = 1000
+MAX_R = 2000
 
-li = [
-    0 for _ in range(2000)
-]
-
-strPoint = 999
-
+# 변수 선언 및 입력
 n = int(input())
-for i in range(n):
-    a,b = map(str,input().split())
-    movePoint = int(a)
-    if b == 'R':
-        for i in range(movePoint+1):
-            li[strPoint+i] += 1
-        strPoint = strPoint+i
-        #print(strPoint)
-    else:
-        for i in range(movePoint+1):
-            li[strPoint-i] += 1
-        strPoint = strPoint-i
-        #print(strPoint)
+segments = []
 
-print(li.count(2))
+# 현재 위치
+cur = 0
+
+for _ in range(n):
+    distance, direction = tuple(input().split())
+    distance = int(distance)
+    
+    if direction == 'L':
+        # 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+        section_left = cur - distance
+        section_right = cur
+        cur -= distance
+    else:
+        # 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+        section_left = cur
+        section_right = cur + distance
+        cur += distance
+    
+    segments.append([section_left, section_right])
+
+print(segments)
+
+checked = [0] * (MAX_R + 1)
+
+for x1, x2 in segments:
+    # OFFSET을 더해줍니다.
+    x1, x2 = x1 + OFFSET, x2 + OFFSET
+    print(x1, x2)
+    
+    # 구간을 칠해줍니다.
+    # 구간 단위로 진행하는 문제이므로
+    # x2에 등호가 들어가지 않음에 유의합니다.
+    for i in range(x1, x2):
+        checked[i] += 1
+
+    
+# 2번 이상 지나간 영역의 크기를 구합니다.
+cnt = 0
+for elem in checked:
+    if elem >= 2:
+        cnt += 1
+print(cnt)
