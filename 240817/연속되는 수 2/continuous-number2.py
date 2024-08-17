6ccf461cc045608c07cb36540e6951009e5e8a79
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-17 23:45:18 +0900

    Title: 연속되는 수 2, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240817/연속되는 수 2/continuous-number2.py
+++ 240817/연속되는 수 2/continuous-number2.py
@@ -1,11 +1,24 @@
-n = int(input())
-cnt = 0
-a = []
-for _ in range(n):
-    a.append(int(input()))
+def find_max_consecutive(numbers):
+    if not numbers:
+        return 0
     
-for i in range(n):
-    if i == 0 or a[i] != a[i - 1]:
-        cnt += 1
+    max_count = 1
+    current_count = 1
+    
+    for i in range(1, len(numbers)):
+        if numbers[i] == numbers[i-1]:
+            current_count += 1
+            max_count = max(max_count, current_count)
+        else:
+            current_count = 1
+    
+    return max_count
 
-print(cnt)
+# 입력 받기
+input_numbers = []
+n = int(input())
+for _ in range(n):
+    input_numbers.append(int(input()))
+
+# 결과 출력
+print(find_max_consecutive(input_numbers))
