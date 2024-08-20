1b93dbfa173fd03278511845bac2ba989fadd17e
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-20 10:33:03 +0900

    Title: 연속되는 수 4, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240820/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,14 @@
+n = int(input())
+li = [int(input()) for _ in range(n)]
+
+ans,cnt = 0,0
+for i in range(n):
+    if i >= 1 and li[i] > li[i-1]:
+        cnt += 1
+    else:
+        cnt = 1
+    
+    #print(li[i],ans,cnt)
+    ans = max(ans,cnt)
+
+print(ans)
