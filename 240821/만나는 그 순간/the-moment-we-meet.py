a9e405457c6406a01b2bc9d6e25017dca6492853
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-21 12:07:59 +0900

    Title: 만나는 그 순간, Time: 255ms, Memory: 45MB, Status: Passed - CodeTree

--- 240821/만나는 그 순간/the-moment-we-meet.py
+++ 240821/만나는 그 순간/the-moment-we-meet.py
@@ -1,55 +1,33 @@
-OFFSET = 100000
+def simulate_movement(moves):
+    path = [100000]  # Start at position 100000
+    for direction, x in moves:
+        if direction == 'L':
+            path.extend(range(path[-1] - 1, path[-1] - x - 1, -1))
+        else:  # direction == 'R'
+            path.extend(range(path[-1] + 1, path[-1] + x + 1))
+    return path
 
-N,M = map(int,input().split())
+def find_meeting_point(path_a, path_b):
+    for i in range(1, min(len(path_a), len(path_b))):
+        if path_a[i] == path_b[i]:
+            return i
+    return -1
 
-A = [0] * (2 * OFFSET + 1)
-B = [0] * (2 * OFFSET + 1)
+# Read input
+N, M = map(int, input().split())
 
-cur = 100000
-cnt = 0
-for _ in range(N):
-    direction, x = map(str,input().split())
-    x = int(x)
-    if direction == 'L':
-        while x >= 1:
-            A[cnt] = cur
-            cur -= 1
-            cnt += 1
-            x -= 1
-    else:
-        while x >= 1:
-            A[cnt] = cur
-            cur += 1
-            cnt += 1
-            x -= 1
+# Read moves for A and B
+moves_a = [input().split() for _ in range(N)]
+moves_b = [input().split() for _ in range(M)]
 
+# Convert string numbers to integers
+moves_a = [(d, int(x)) for d, x in moves_a]
+moves_b = [(d, int(x)) for d, x in moves_b]
 
-cur = 100000
-cnt = 0
-for _ in range(M):
-    direction, x = map(str,input().split())
-    x = int(x)
-    if direction == 'L':
-        while x >= 1:
-            B[cnt] = cur
-            cur -= 1
-            cnt += 1
-            x -= 1
-    else:
-        while x >= 1:
-            B[cnt] = cur
-            cur += 1
-            cnt += 1
-            x -= 1
-#[1000, 999, 998, 999, 1000, 1001, 1002, 1003, 1002, 1001, 1000, 999, 998, 0, 0, 0, 0, 0, 0, 0]
-#[1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 0, 0, 0, 0, 0, 0, 0]
-# print(A[:20])
-# print(B[:20])
-for i in range(1,2002):
-    if A[i] == B[i]:
-        if A[i] == 0 and B[i] == 0:
-            print(-1)
-            break
-        else:
-            print(i)
-            break
+# Simulate movements
+path_a = simulate_movement(moves_a)
+path_b = simulate_movement(moves_b)
+
+# Find meeting point
+result = find_meeting_point(path_a, path_b)
+print(result)
