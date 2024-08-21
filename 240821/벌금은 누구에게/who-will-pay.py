a8aac6a276f36f9747e398a3d7cf570041b303e1
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-21 12:55:32 +0900

    Title: 벌금은 누구에게, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240821/벌금은 누구에게/who-will-pay.py
+++ 240821/벌금은 누구에게/who-will-pay.py
@@ -10,7 +10,7 @@ for _ in range(b):
 for i in li_in:
     li[i-1] += 1
     #print(li)
-    if li[i-1] == 3:
+    if li[i-1] == c:
         setting = False
         print(li.index(li[i]))
         break
