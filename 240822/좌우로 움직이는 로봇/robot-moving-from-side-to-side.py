ef1f1d779c8067d9eefb00e0245cbeb4f48b2b6f
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-22 15:06:46 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240822/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240822/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,5 +1,5 @@
 def simulate_movement(moves):
-    path = [100000]  # Start at position 100000
+    path = [100]  # Start at position 100000
     for x , direction in moves:
         if direction == 'L':
             path.extend(range(path[-1] - 1, path[-1] - x - 1, -1))
@@ -9,9 +9,13 @@ def simulate_movement(moves):
 
 def find_meeting_point(path_a, path_b):
     cnt = 0
-    for i in range(1, min(len(path_a), len(path_b))):
-        if path_a[i] == path_b[i]:
-            cnt += 1
+    for i in range(1, max(len(path_a), len(path_b))):
+        try:
+            if (path_a[i-1] != path_b[i-1]) and (path_a[i] == path_b[i]):
+                cnt += 1
+        except:
+            if (path_a[len(path_a)-1] == path_b[i]):
+                cnt += 1
     return cnt
 
 # Read input
@@ -29,6 +33,11 @@ moves_b = [(int(x), d) for x, d in moves_b]
 path_a = simulate_movement(moves_a)
 path_b = simulate_movement(moves_b)
 
+#print(path_a)
+#print(path_b)
 # Find meeting point
 result = find_meeting_point(path_a, path_b)
-print(result-1)
+print(result)
+
+#[100, 99, 98, 97, 98, 99, 100, 101, 102, 101, 102, 103]
+#[100, 101, 102, 103, 104, 103, 102, 101, 100, 101, 102, 103, 104, 103, 102]
