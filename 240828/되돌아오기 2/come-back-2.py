152db0b6ccb8042931df8d4a4f64129f91d617df
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-08-28 16:06:12 +0900

    Title: 되돌아오기 2, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240828/되돌아오기 2/come-back-2.py
@@ -0,0 +1,30 @@
+cmd = list(input())
+
+    # E S W N
+dx = [1,0,-1,0] 
+dy = [0,-1,0,1] 
+
+x,y = 0,0 #원점
+cur_x , cur_y = 0,0 #현재위치
+
+#입력 받은 대로 움직이기
+stopS = False
+cnt = 0
+dir_num = 3
+
+for D in cmd:
+    if D == 'F': # 직진
+        cur_x, cur_y = cur_x + dx[dir_num] , cur_y + dy[dir_num]
+        cnt += 1
+        if cur_x == x and cur_y == y:
+            break
+    elif D == 'L': # 반시계
+        dir_num = (dir_num -1 + 3) % 4
+        cnt += 1
+    elif D == 'R': # 시계
+        dir_num = (dir_num + 1) % 4
+        cnt += 1
+    else:
+        print("wrong Input")
+
+print(cnt)
