4fd272cd552dd1a1bd93ba4e805cc916cd4a7885
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-09-10 13:26:08 +0900

    Title: 이상한 진수 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,20 @@
+N = list(map(int,input()))
+
+def binaryToDecimal(n):
+    total = 0
+    for i in range(0,len(n)):
+        #print(n[i],(2 ** (len(n)-1-i)))
+        total += n[i] * (2 ** (len(n)-1-i))
+    return total
+
+maxNumber = 0
+
+#print(binaryToDecimal(N))
+
+for i in range(1,len(N)):
+    test = N
+    test[i] = 0 if test[i] == 1 else 1
+    #print(test,binaryToDecimal(test))
+    maxNumber = binaryToDecimal(test) if maxNumber <= binaryToDecimal(test) else maxNumber
+
+print(maxNumber)
