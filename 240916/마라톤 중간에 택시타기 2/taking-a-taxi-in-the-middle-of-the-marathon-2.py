ef297efdbcddd89c649b91bbec9b768c1e9946a6
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-09-16 16:22:34 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240916/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240916/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -6,25 +6,19 @@ def calculate_distance(x1, x2):
     return abs(x1[0] - x2[0]) + abs(x1[1] - x2[1])
 
 N = int(input())
-checkPoints = [
+checkPoint = [
     list(map(int, input().split()))
     for _ in range(N)
 ]
 
 answer = INT_MAX
 
-# 첫 번째와 마지막 체크포인트는 항상 포함
-# 중간 체크포인트 중 하나만 건너뛰는 경우를 모두 고려
-for skip in range(1, N-1):  # 1부터 N-2까지의 인덱스 (첫 번째와 마지막을 제외한 중간 체크포인트)
+for j in range(1, N-1):  # 첫 번째와 마지막 체크포인트는 제거하지 않음
     result = 0
-    prev = checkPoints[0]  # 시작점
+    checkPoint_new = checkPoint[:j] + checkPoint[j+1:]  # j번째 체크포인트를 제거
     
-    for i in range(1, N):
-        if i == skip:
-            continue  # 이 체크포인트를 건너뛴다
-        curr = checkPoints[i]
-        result += calculate_distance(prev, curr)
-        prev = curr
+    for k in range(1, len(checkPoint_new)):
+        result += calculate_distance(checkPoint_new[k-1], checkPoint_new[k])
     
     answer = min(answer, result)
 
