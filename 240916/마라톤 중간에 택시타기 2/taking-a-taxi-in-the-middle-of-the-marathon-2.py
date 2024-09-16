bc1913cf99409c97fcaa959bc84a9d2bb033de91
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-09-16 16:24:55 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240916/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240916/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -2,24 +2,27 @@ import sys
 
 INT_MAX = sys.maxsize
 
-def calculate_distance(x1, x2):
-    return abs(x1[0] - x2[0]) + abs(x1[1] - x2[1])
+def calculate_distance(x1,x2):
+    return abs(x1[0]-x2[0]) + abs(x1[1]-x2[1])
 
 N = int(input())
 checkPoint = [
-    list(map(int, input().split()))
+    list(map(int,input().split()))
     for _ in range(N)
 ]
 
 answer = INT_MAX
 
-for j in range(1, N-1):  # 첫 번째와 마지막 체크포인트는 제거하지 않음
+for j in range(1,N-1):
+    #print(j)
     result = 0
-    checkPoint_new = checkPoint[:j] + checkPoint[j+1:]  # j번째 체크포인트를 제거
-    
-    for k in range(1, len(checkPoint_new)):
-        result += calculate_distance(checkPoint_new[k-1], checkPoint_new[k])
-    
-    answer = min(answer, result)
+    checkPoint_new = checkPoint[:]
+    checkPoint_new.pop(j)
+    #print(checkPoint_new)
+    for k in range(1,len(checkPoint_new)):        
+        #print(checkPoint_new[k-1],checkPoint_new[k])
+        result += calculate_distance(checkPoint_new[k-1],checkPoint_new[k])
+    if answer >= result:
+        answer = result
 
 print(answer)
