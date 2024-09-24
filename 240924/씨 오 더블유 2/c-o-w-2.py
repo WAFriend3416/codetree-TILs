e58005d713c7d778f51c0baa6012fa5d9a2a3d00
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-09-24 14:14:04 +0900

    Title: 씨 오 더블유 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/씨 오 더블유 2/c-o-w-2.py
+++ 240924/씨 오 더블유 2/c-o-w-2.py
@@ -7,14 +7,16 @@ for i in range(0,N-2):
     if COW_list[i] != 'C':
         continue
     else:
-        for j in range(1,N-1):
+        for j in range(i,N-1):
             if COW_list[j] != 'O':
                 continue
             else:
-                for k in range(2,N):
+                for k in range(j,N):
                     if COW_list[k] != 'W':
                         continue
                     else:
-                        cnt += 1
+                        if COW_list[i] == 'C' and COW_list[j] == 'O' and COW_list[k] == 'W':
+                            #print(i,j,k)
+                            cnt += 1
 
 print(cnt)
