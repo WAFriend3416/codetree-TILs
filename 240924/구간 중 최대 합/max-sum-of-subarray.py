881af52c431739bba921569a2653ffbe99d37526
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-09-24 14:30:42 +0900

    Title: 구간 중 최대 합, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240924/구간 중 최대 합/max-sum-of-subarray.py
+++ 240924/구간 중 최대 합/max-sum-of-subarray.py
@@ -3,11 +3,13 @@ numbers = list(map(int,input().split()))
 
 
 mV = 0
-for i in range(0,N-2):
+for i in range(0,N-M):
     result = 0
-    result = numbers[i] + numbers[i+1] +  numbers[i+2]
-    if mV <= result:
-        mV =result
+   
+    for j in range(i,i+M):
+        #print(i+j)
+        result += numbers[j]
+    mV = max(mV,result)
 
 
 print(mV)
