e15b39078211d7578a50f541008599abd8096993
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-10-04 14:15:21 +0900

    Title: 개발자의 능력 3, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241004/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,14 @@
+nums = list(map(int,input().split()))
+
+sum0 = sum(nums)
+minV = 1000000000000000
+
+for i in range(len(nums)-2):
+    for j in range(1,len(nums)-1):
+        for k in range(2,len(nums)):
+            sum1 = nums[i] + nums[j] + nums[k]
+            sum2 = sum0 - sum1
+            minV = min(minV,abs(sum1-sum2))
+
+
+print(minV)
