23523ac0fc1efe807508ecbc509f385aa4735b7e
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-10-04 14:20:36 +0900

    Title: 개발자의 능력 3, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 241004/개발자의 능력 3/ability-of-developer-3.py
+++ 241004/개발자의 능력 3/ability-of-developer-3.py
@@ -6,9 +6,15 @@ minV = 1000000000000
 for i in range(len(nums)-2):
     for j in range(1,len(nums)-1):
         for k in range(2,len(nums)):
-            sum1 = nums[i] + nums[j] + nums[k]
-            sum2 = sum0 - sum1
-            minV = min(minV,abs(sum1-sum2))
-
+            if i == j or i == k or j == k:
+                # print(nums[i] , nums[j] , nums[k])
+                continue
+            else:
+                sum1 = nums[i] + nums[j] + nums[k]
+                sum2 = sum0 - sum1
+                # if abs(sum1-sum2) == 4:
+                #     print(nums[i] , nums[j] , nums[k])
+                minV = min(minV,abs(sum1-sum2))
+            #print(minV)
 
 print(minV)
