26152f693874c7bd230797a55ae1de3c3bacd7dd
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-10-06 17:37:12 +0900

    Title: 아름다운 수열 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241006/아름다운 수열 2/beautiful-sequence-2.py
+++ 241006/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,17 +1,17 @@
+from itertools import combinations, permutations
+
 N,M = map(int,input().split())
 li = list(map(int,input().split()))
 target = list(map(int,input().split()))
+
+combi = list(combinations(target, M))
 
 answer = 0
 
 for i in range(len(li)-M+1):
     cnt = 0
     #print(li[i:i+M])
-    for j in target:
-        if j in li[i:i+M]:
-            cnt += 1
-    if cnt == M:
-        #print(li[i:i+M],target)
+    if li[i:i+M] in combi:
         answer += 1
 
 print(answer)
