ee0e0c34eca5a2e020c9b6b5bacc9a47badd0176
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-10-07 11:42:07 +0900

    Title: 아름다운 수열 2, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241007/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,22 @@
+from itertools import combinations, permutations
+
+N,M = map(int,input().split())
+li = list(map(int,input().split()))
+target = list(map(int,input().split()))
+
+#target_case = list(permutations(target,M))
+#print(target_case)
+answer = 0
+
+for i in range(0,len(li)-M+1):
+    cnt = 0
+    target_case = target[:]
+    for j in li[i:i+M]:
+        #print(j,end = ' ')
+        if j in target_case:
+            target_case.pop(target_case.index(j))
+    #print()
+    if len(target_case) == 0:
+       answer += 1
+
+print(answer)
