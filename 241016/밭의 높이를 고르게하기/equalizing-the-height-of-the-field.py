6e843361a2d07a791902fb90f74580867c472861
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2024-10-16 11:09:45 +0900

    Title: 밭의 높이를 고르게하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 241016/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -2,13 +2,14 @@ N,H,T = map(int,input().split())
 base = list(map(int,input().split()))
 
 minV = 1000000000000000
-for i in range(0,N-T):
+for i in range(0,N-T+1):
     compV = 0
-    target = base[i:i+T] 
+    target = base[i:i+T]
+    #print(target) 
     if (sum(target) % H) == 0:
         for j in target:
             compV += abs(H-j)
-    
+    #print(compV)
     minV = min(compV,minV)
 
 
